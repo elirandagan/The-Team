df4cecb3351efe50d3e02f5d85aab5dfe55524e4
elirandagan <57113832+elirandagan@users.noreply.github.com>
2019-12-09 15:46:39 +0200

    the files that works on the data bases

---
+++ user_db_file.py
@@ -0,0 +1,14 @@
+import json
+from user_database import user_list
+
+
+def save_user_db_file(user_list):
+    with open("user_db_file.txt", 'w') as file:
+        json.dump(user_list, file, indent=2)
+
+
+def load_user_db_file():
+    with open("user_db_file.txt", 'r') as file:
+        user_list = json.load(file)
+
+
