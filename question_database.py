feb788c1b56061c3edf12162769b56c1564baf9f
Eliran Dagan <elirandagan123@gmail.com>
2019-12-09 12:42:48 +0200

    upload

---
+++ question_database.py
@@ -0,0 +1,24 @@
+from classes import Question
+
+ques = {"question_serial_number": ["main_subject", "sub_subject", "difficulty_level", "solution", "year",
+                                   "semester", "exam_date", "format"]}
+
+""" Need to be checked in the sending if its a Question class"""
+
+
+def add_question(q: Question):
+    ques.update({q.question_serial_number: [q.main_subject, q.sub_subject, q.difficulty_lvl, q.solution, q.year
+        , q.semester, q.exam_date, q.format_q]})
+
+
+def del_question(string: str):
+    for key in ques:
+        if string == key:
+            ques.pop(key)
+            break
+        else:
+            return -1
+
+
+def update_question():  # todo implement
+    pass
